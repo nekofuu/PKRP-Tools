e1bf7e079df6f6f1614ff93df7187f71e11777e3
nekofuu <nekofuuhd@gmail.com>
2019-06-15 08:43:31 -0400

    Fixed timer
    Timer interval wasn't clearing because someone wasn't using the correct
    variable.

--- assets/js/app.js
+++ assets/js/app.js
@@ -93,17 +93,19 @@ function timerInit() {
         let now = new Date().getTime();
         let distance = end.getTime() - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        timerCounter.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-
         if (distance < 0) {
-            clearInterval(x);
+            clearInterval(countdown);
             timerCounter.innerHTML = "EXPIRED";
+        } else {
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            timerCounter.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
         }
+
+        
     }, 1000)
 }
 
